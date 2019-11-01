690e816246985858dc2a55bb917ea9d616e5e594
Pranav Srivastava <pranavsrivastava@Pranavs-MacBook-Pro.local>
2019-11-01 09:56:09 +0100

    updates

--- utils.py
+++ utils.py
@@ -3,14 +3,17 @@ import requests
 def pickanswer(text,tonumber):
     file = open("sample_intents.csv", "r")
 
+    #check if you are getting the expected output, adjust your delimitter
     print("text>>",text)
-    text_fields = text.split("+")
+    text_fields = text.split(" ")
+    #text_fields = text.split("+")
+    print("text fields",text_fields)
     input_words = []
 
     for i in range(0, len(text_fields)):
         print(text_fields[i])
         input_words.append(text_fields[i])    
-
+    print("input words: ",input_words)
     predict_answer = {}
 
     for line in file:
@@ -29,17 +32,17 @@ def pickanswer(text,tonumber):
     print("dict>>>",predict_answer)
     answer = max(predict_answer, key=predict_answer.get)
     print("predict>>",answer)    
-    #sendSMS(answer,tonumber)
+    sendSMS(answer,tonumber)
 
 
 def sendSMS(text,tonumber):
 
     url = "https://api-prd.kpn.com/communication/nexmo/sms/send"
 
-    payload = "from=VIRTUAL_NUMBER&to="+tonumber+"&text="+text
+    payload = "from=3197010240299&to="+tonumber+"&text="+text
     headers = {
         'Content-Type': "application/x-www-form-urlencoded",
-        'Authorization': "Bearer YOUR_BEARE_TOKEN",
+        'Authorization': "Bearer xtCvqnBFHdHgMPCjA0gVvgRGzHEU",
         'Accept': "*/*",
         'Cache-Control': "no-cache",
         'Host': "api-prd.kpn.com",
